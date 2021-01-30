40724b672cc9a0670a4f6bc2d6af71394dd7ab5b
Renato França <renatofrancarpmf@gmail.com>
2021-01-30 02:41:40 -0300

    save button removes first list item
    Co-authored-by: Maíra Abianna <mymalp@gmail.com>

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function onSubmit() {
-    alert('estou escondido');
+    const list = document.getElementById('list');
+
+    const li = Array.from(list.getElementsByTagName('li'));
+
+    li.shift();
+
+    list.innerHTML = `
+        <li>${li[0].innerHTML}</li>
+        <li>${li[1].innerHTML}</li>
+        <li>${li[2].innerHTML}</li>
+        <li>${li[3].innerHTML}</li>
+    `;
 }
