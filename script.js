5954195486f8b26c458ee34f8cc7380d9fdd65f8
Renato França <renatofrancarpmf@gmail.com>
2021-02-04 02:07:50 -0300

    empty validation added and empty field text after add a new task
    Co-authored-by: Maíra Abianna <mymalp@gmail.com>

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 function onSubmit() {
+    // Pega a referência da lista
     const list = document.getElementById('list');
+    // Pega a referência do input de texto
     const text = document.getElementById('text').value;
 
+    // Valida se o campo de texto está vazio
+    if (text == '') {
+        return;
+    }
+
+    // Converte a lista para array
     const li = Array.from(list.getElementsByTagName('li'));
 
+    // Adiciona novo item no início da lista
     list.innerHTML = `<li>${text}</li>`;
+
+    // Percorre a lista li
     for (let index = 0; index < li.length; index++) {
+        // Incrementa a lista concatenando o valor que já existe com o novo
         list.innerHTML = list.innerHTML + `<li>${li[index].innerHTML}</li>`;
     }
 
+    // Limpa o campo de texto após adicionar nova task
+    document.getElementById('text').value = '';
 }
