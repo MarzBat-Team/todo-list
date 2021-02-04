0327dc71f1f628bb5d93e8e9284f0822af61d36d
Renato França <renatofrancarpmf@gmail.com>
2021-02-04 02:28:57 -0300

    todo list finish added
    Co-authored-by: Maíra Abianna <mymalp@gmail.com>

--- script.js
+++ script.js
@@ -13,14 +13,23 @@ function onSubmit() {
     const li = Array.from(list.getElementsByTagName('li'));
 
     // Adiciona novo item no início da lista
-    list.innerHTML = `<li>${text}</li>`;
+    list.innerHTML = `<li id="new-task">${text} <button onClick="finishTask(event)" type="button">✅ concluir</button></li>`;
 
     // Percorre a lista li
     for (let index = 0; index < li.length; index++) {
         // Incrementa a lista concatenando o valor que já existe com o novo
-        list.innerHTML = list.innerHTML + `<li>${li[index].innerHTML}</li>`;
+        list.innerHTML = list.innerHTML + `<li id="task${index}">${li[index].innerHTML}</li>`;
     }
 
     // Limpa o campo de texto após adicionar nova task
     document.getElementById('text').value = '';
+}
+
+// Remove a task concluída
+function finishTask(event) {
+    // Pega o id do element anterior ao botão
+    const id = event.target.parentElement.id;
+
+    // Remove o elemento
+    document.getElementById(id).remove();
 }
